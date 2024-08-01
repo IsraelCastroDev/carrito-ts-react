42edf288289e5c8eee34215aa1cf0a30a0daf58f
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 12:25:19 -0500

    Agregando las devtools de zustand

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,6 +1,9 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { createProductsSlice, ProductSliceType } from "./slices/productsSlice";
 
-export const useAppStore = create<ProductSliceType>((...a) => ({
-  ...createProductsSlice(...a),
-}));
+export const useAppStore = create<ProductSliceType>()(
+  devtools((...a) => ({
+    ...createProductsSlice(...a),
+  }))
+);
