d012581af70a6692c8901c5c2162e55017dd43cd
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 12:23:27 -0500

    Agregando enlaces

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { IconCart } from "./icons/IconCart";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Riplay</h1>
+      <Link to={"/"}>
+        <h1>Riplay</h1>
+      </Link>
+
+      <Link to="/carrito">
+        <IconCart />
+      </Link>
     </header>
   );
 }
