01aa1d5224e9d64a1ccfeef2cb74867e2cec9225
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 12:23:14 -0500

    Función para obtener un solo producto

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,9 +1,18 @@
 import { ax } from "../config/axios";
-import { ProductsAPIResponse } from "../schemas";
+import { Product, ProductAPIResponse, ProductsAPIResponse } from "../schemas";
 
 export const getAllProducts = async () => {
   const { data } = await ax("/products");
   const validateProductData = ProductsAPIResponse.safeParse(data);
+  if (validateProductData.success) {
+    return validateProductData.data;
+  }
+};
+
+export const getSingleProduct = async (id: Product["id"]) => {
+  const { data } = await ax(`/products/${id}`);
+  const validateProductData = ProductAPIResponse.safeParse(data);
+
   if (validateProductData.success) {
     return validateProductData.data;
   }
