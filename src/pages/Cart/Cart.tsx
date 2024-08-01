404a3adbd91e6a073f2f2c0d97cd8ffb384dc71b
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 15:38:40 -0500

    Mostrando el contenido de cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -2,14 +2,17 @@ import { useMemo } from "react";
 import { usePersistedStore } from "../../store/usePersistedStore";
 import style from "./Cart.module.css";
 import { formatCurrency } from "../../helpers";
+import { Link } from "react-router-dom";
+import { DeleteIcon } from "../../components/icons/DeleteIcon";
 
 function Cart() {
   const cart = usePersistedStore((state) => state.cart);
+  const deleteProduct = usePersistedStore((state) => state.deleteProduct);
   const cartIsEmpty = useMemo(() => cart.length === 0, [cart]);
 
   return (
     <section className={style["cart-container"]}>
-      <div>
+      <div className={style["cart-info"]}>
         <h1>Carrito de compras</h1>
         {!cartIsEmpty ? (
           <ul className={style["cart-items"]}>
@@ -19,10 +22,20 @@ function Cart() {
                   <img src={product.image} alt="" />
                   <p>{product.title}</p>
                 </div>
-                <div>
-                  <p>{product.quantity}</p>
-                  <p>{formatCurrency(product.price)}</p>
-                  <p>{formatCurrency(product.price * product.quantity)}</p>
+                <div className={style.actions}>
+                  <div>
+                    <p>Precio: {formatCurrency(product.price)}</p>
+                    <p>
+                      Subtotal:{" "}
+                      {formatCurrency(product.price * product.quantity)}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => deleteProduct(product.id)}
+                    className={style["icon-delete"]}
+                  >
+                    <DeleteIcon />
+                  </button>
                 </div>
               </li>
             ))}
@@ -31,6 +44,7 @@ function Cart() {
           <p>Tu carrito esta vacio</p>
         )}
       </div>
+
       <div className={style.resume}>
         <h2 className={style.title}>Resumen</h2>
         <div>
@@ -39,6 +53,9 @@ function Cart() {
         </div>
 
         <button>Pagar pedido</button>
+        <Link className={style.link} to="/">
+          Seguir comprando
+        </Link>
       </div>
     </section>
   );
