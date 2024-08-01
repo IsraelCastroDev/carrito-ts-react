5a1928fa99a3ddf55062250144dec0bc6a874f0a
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 11:18:49 -0500

    Validando datos con zod e infiriendo tipos

---
+++ src/schemas/index.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const ProductAPIResponse = z.object({
+  id: z.number(),
+  title: z.string(),
+  price: z.number(),
+  category: z.string(),
+  description: z.string(),
+  image: z.string(),
+});
+export const ProductsAPIResponse = z.array(ProductAPIResponse);
+
+export type Product = z.infer<typeof ProductAPIResponse>;
