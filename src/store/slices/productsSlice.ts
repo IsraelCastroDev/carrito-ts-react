f5faab01d5cff2ab75cdd1a4005f0cfac903fd58
Isreal Castro <israelcastro.dev@gmail.com>
2024-08-01 12:25:43 -0500

    Creando estado de singleProduct

--- src/store/slices/productsSlice.ts
+++ src/store/slices/productsSlice.ts
@@ -1,18 +1,29 @@
 import { StateCreator } from "zustand";
 import { Product } from "../../schemas";
-import { getAllProducts } from "../../api/products";
+import { getAllProducts, getSingleProduct } from "../../api/products";
 
 export type ProductSliceType = {
   products: Product[];
+  singleProduct: Product;
   getProducts: () => Promise<void>;
+  getProductById: (productId: Product["id"]) => Promise<void>;
+  clearStateSingleProduct: () => void;
 };
 
 export const createProductsSlice: StateCreator<ProductSliceType> = (set) => ({
   products: [],
+  singleProduct: {} as Product,
   getProducts: async () => {
     const products = await getAllProducts();
     set({
       products,
     });
   },
+  getProductById: async (id) => {
+    const product = await getSingleProduct(id);
+    set({
+      singleProduct: product,
+    });
+  },
+  clearStateSingleProduct: () => set({ singleProduct: {} as Product }),
 });
